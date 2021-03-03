714bbee409dee1271550e504dae9319f98942be3
EliCUBE <79873558+EliCUBE@users.noreply.github.com>
2021-03-03 12:10:20 -0800

    Add files via upload

---
+++ schoolthing1.py
@@ -0,0 +1,31 @@
+"""
+March 2, 2021
+
+Use this document to record your answers to the Parson's Problems
+shared in class.
+
+Remember: the idea behind a Parson's Problem is that you are
+shown completely correct code that is presented OUT OF ORDER.
+When arranged correctly, the code does what the Parson's Problem
+says it should do.
+
+"""
+
+
+#Write your answer to Parson's Problem #1 below:
+for x in (1,2,3,4,5,6,7,8,9,10):
+    print ("hello world")
+
+#Write your answer to Parson's Problem #2 below:
+MyList = []
+MyList.append ("apples")
+MyList.append ("pineapples")
+print (MyList)
+
+#Write your answer to Parson's Problem #3 below:
+x = input("type a number               ")
+x = int(x)
+if x > 0:
+    print("thats more than nothing")
+else:
+    print("thats a small number")
